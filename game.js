0b03b716d71cb274bb16ce5f14d55dd774179b5e
drebob24 <abobbit@gmail.com>
2023-05-12 18:14:47 +0300

    Create game() function
    Create the game() function to play 5 Win/Lose rounds, Draws are not
    counted towards the total. A tally is kept and the overall winner
    is announced at the end.
    
    Clean up variables used earlier to remove variable/parameter confusion

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 /* Ask for a user insput
-Generate computer choice
-Compare User vs Computer
-Return Win/Lose */
+Generate computer choice XX
+Compare User vs Computer XX
+Return Win/Lose XX
+Play 5 rounds XX
+    keep tally XX
+    declare overall winner XX
+*/
 
-let compChoice = getRandomInt();
-let computerSelection = getComputerChoice();
+let playerWins = 0;
+let computerWins = 0;
+let drawCount = 0;
 
 function getComputerChoice(){
-    compChoice = getRandomInt();
+    let compChoice = getRandomInt();
     console.log(compChoice);
     if (compChoice === 0) {
         return "Rock";
@@ -23,48 +28,84 @@ function getRandomInt() {
     return Math.floor(Math.random() * 3);
 }
 
-let playerSelection = "";
-
-
 function playRound(playerSelection, computerSelection){
     playerSelection = playerSelection.toLowerCase();
     console.log(playerSelection);
     
     if (playerSelection === "rock"){
         if (computerSelection === "Paper"){
+            computerWins++;
             return "You Lose! Paper beats Rock.";
         }
         else if (computerSelection === "Scissors"){
+            playerWins++;
             return "You Win! Rock beats Scissors";
         }
         else
-            return "Draw! Stuck between a Rock and, well, another Rock";
+            drawCount++;
+            return "Draw! Go Again.";
 
     }
     else if (playerSelection === "paper"){
         if (computerSelection === "Scissors"){
+            computerWins++;
             return "You Lose! Scissors beats Paper.";
         }
         else if (computerSelection === "Rock"){
+            playerWins++;
             return "You Win! Paper beats Rock";
         }
         else
-            return "Draw! Ouch, papercut.";
+            drawCount++;
+            return "Draw! Go Again.";
         
     }
     else if (playerSelection === "scissors"){
         if (computerSelection === "Rock"){
+            computerWins++;
             return "You Lose! Rock beats Scissors.";
         }
         else if (computerSelection === "Paper"){
+            playerWins++;
             return "You Win! Scissors beats Paper";
         }
         else
-            return "Draw! Neither of you is the sharpest scissor in the drawer.";
+            drawCount++
+            return "Draw! Go Again.";
         
     }
     else
         return "Please choose Rock, Paper, or Scissors";
+}
 
+function game(){
+    playerWins = 0;
+    computerWins = 0;
+    drawCount = 0;
+    for (let i = 1; i <= 5; i++){
+        //Call to get user input
+        let playerSelection = "PAPER";
+        //Call to get computerChoice
+        let computerSelection = getComputerChoice();
+        //Call to Play Round
+        console.log(playRound(playerSelection, computerSelection));
+        //Print Result
+        console.log("Player: " + playerWins);
+        console.log("Computer: " + computerWins);
+        //increase Player or Computer Win count --handled in playRound
+        //To make it a true best of 5, Draws are not counted
+        if (drawCount > 0) {
+            i--;
+            drawCount--;
+        }
+    }
 
+    console.log(determineWinner(playerWins, computerWins));
+}
+
+function determineWinner($playerWins, $computerWins){
+    if ($playerWins > $computerWins)
+        return "Congrats you won!"
+    else
+        return "Computer reigns supreme!"
 }
