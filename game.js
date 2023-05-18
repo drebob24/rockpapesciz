64eac94569657a526cdfa78a965602537aef642c
drebob24 <abobbit@gmail.com>
2023-05-18 16:56:11 +0300

    Integrate round counting and determine a winner
    Also clean up code.
    
    Still needed: make the game stop after 5 wins and a reset option

--- game.js
+++ game.js
@@ -1,15 +1,13 @@
-/* Ask for a user insput XX
-Generate computer choice XX
-Compare User vs Computer XX
-Return Win/Lose XX
-Play 5 rounds XX
-    keep tally XX
-    declare overall winner XX
-*/
-
 let playerWins = 0;
 let computerWins = 0;
 let drawCount = 0;
+let computerSelection = "";
+
+const results = document.querySelector(".results");
+const compChoice = document.createElement('p');
+const roundResult = document.createElement('p');
+const tally = document.createElement('p');
+const victory = document.createElement('p');
 
 function getComputerChoice(){
     let compChoice = getRandomInt();
@@ -27,9 +25,8 @@ function getRandomInt() {
     return Math.floor(Math.random() * 3);
 }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-
+function playRound(playerSelection){
+    computerSelection = getComputerChoice();
     if (playerSelection === "rock"){
         if (computerSelection === "Paper"){
             computerWins++;
@@ -72,87 +69,39 @@ function playRound(playerSelection, computerSelection){
             return "Draw! Go Again.";
         
     }
-    else
-        drawCount++; //To decrease the Round (i variable) in game()
-        return "Please choose Rock, Paper, or Scissors";
-
 }
 
-function game(){
-    playerWins = 0;
-    computerWins = 0;
-    drawCount = 0;
-    for (let i = 1; i <= 5; i++){
-        let playerSelection = prompt("Whatcha choosin? Best of 5");
-        let computerSelection = getComputerChoice();
-        // console.log(playRound(playerSelection, computerSelection));
-        // console.log("Player: " + playerWins);
-        // console.log("Computer: " + computerWins);
-
-        /* More climactic game setting. Computer cheats to force 5 rounds of play*/
-        if (playerWins === 2 && i < 5){
-            console.log(computerWinsRound(playerSelection))
-            console.log("Player: " + playerWins);
-            console.log("Computer: " + computerWins);
-        }
-        else {
-            console.log(playRound(playerSelection, computerSelection));
-            console.log("Player: " + playerWins);
-            console.log("Computer: " + computerWins);
-        }
-    
-
-
-        //If either player gets to 3 wins before 5 rounds, they win the set so the loop needs
-        //to end
-        if (playerWins === 3 || computerWins === 3){
-            i = 6;
-        }
-
-        //To make it a true best of 5, Draws are not counted
-        if (drawCount > 0) {
-            i--;
-            drawCount--;
-        }
+function checkIfWinner(){
+    if (playerWins === 5 || computerWins === 5){
+        victory.textContent = determineWinner();
+        results.appendChild(victory);
     }
 
-    console.log(determineWinner(playerWins, computerWins));
 }
 
-function determineWinner($playerWins, $computerWins){
-    if ($playerWins > $computerWins)
+function determineWinner(){
+    if (playerWins > computerWins)
         return "Congrats you won!"
     else
         return "Computer reigns supreme!"
 }
 
-function computerWinsRound(playerSelection){
-    playerSelection = playerSelection.toLowerCase();
-
-    if (playerSelection === "rock"){
-        computerWins++;
-        return "You Lose! Paper beats Rock.";
-    }
-    else if (playerSelection === "paper"){
-        computerWins++;
-        return "You Lose! Scissors beats Paper.";
-    }
-    else if (playerSelection === "scissors"){
-        computerWins++;
-        return "You Lose! Rock beats Scissors.";
-    }
-    else
-        drawCount++; //To decrease the Round (i variable) in game()
-        return "Please choose Rock, Paper, or Scissors";
-}
-
-//Game UI
-const btns = document.querySelectorAll('button');
+const btns = document.querySelectorAll('.choice');
 btns.forEach(choice => {
     choice.addEventListener('click', (e) => {
-        const results = document.querySelector(".results");
-        const roundResult = document.createElement('p');
-        roundResult.textContent = playRound(e.target.id, "Paper");
-        results.appendChild(roundResult);
+        let outcome = playRound(e.target.id); 
+        outputResult(outcome);
+        checkIfWinner();
     });   
 });
+
+function outputResult(roundOutcome){    
+    compChoice.textContent = "The Computer chose: " + computerSelection;
+    results.appendChild(compChoice);
+    
+    roundResult.textContent = roundOutcome;
+    results.appendChild(roundResult);
+    
+    tally.textContent = (" Player: " + playerWins + " Computer: " + computerWins);
+    results.appendChild(tally);
+}
