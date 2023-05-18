7eaff3148bcb84c459ce42909d9b8ffc4ae5699d
drebob24 <abobbit@gmail.com>
2023-05-18 17:31:18 +0300

    Create RESET function
    Game now locks after a win and can be reset.
    
    playRound function cleaned up.

--- game.js
+++ game.js
@@ -1,6 +1,5 @@
 let playerWins = 0;
 let computerWins = 0;
-let drawCount = 0;
 let computerSelection = "";
 
 const results = document.querySelector(".results");
@@ -8,6 +7,9 @@ const compChoice = document.createElement('p');
 const roundResult = document.createElement('p');
 const tally = document.createElement('p');
 const victory = document.createElement('p');
+
+roundResult.textContent = 'Make your choice';
+results.appendChild(roundResult);
 
 function getComputerChoice(){
     let compChoice = getRandomInt();
@@ -28,53 +30,61 @@ function getRandomInt() {
 function playRound(playerSelection){
     computerSelection = getComputerChoice();
     if (playerSelection === "rock"){
-        if (computerSelection === "Paper"){
-            computerWins++;
-            return "You Lose! Paper beats Rock.";
-        }
-        else if (computerSelection === "Scissors"){
-            playerWins++;
-            return "You Win! Rock beats Scissors";
-        }
-        else
-            drawCount++;
-            return "Draw! Go Again.";
-
+        return rockLogic();
     }
     else if (playerSelection === "paper"){
-        if (computerSelection === "Scissors"){
-            computerWins++;
-            return "You Lose! Scissors beats Paper.";
-        }
-        else if (computerSelection === "Rock"){
-            playerWins++;
-            return "You Win! Paper beats Rock";
-        }
-        else
-            drawCount++;
-            return "Draw! Go Again.";
-        
+        return paperLogic();      
     }
     else if (playerSelection === "scissors"){
-        if (computerSelection === "Rock"){
-            computerWins++;
-            return "You Lose! Rock beats Scissors.";
-        }
-        else if (computerSelection === "Paper"){
-            playerWins++;
-            return "You Win! Scissors beats Paper";
-        }
-        else
-            drawCount++;
-            return "Draw! Go Again.";
-        
+        return scissorsLogic();       
     }
+}
+
+function rockLogic(){
+    if (computerSelection === "Paper"){
+        computerWins++;
+        return "You Lose! Paper beats Rock.";
+    }
+    else if (computerSelection === "Scissors"){
+        playerWins++;
+        return "You Win! Rock beats Scissors";
+    }
+    else
+        return "Draw! Go Again.";
+}
+
+function paperLogic(){
+    if (computerSelection === "Scissors"){
+        computerWins++;
+        return "You Lose! Scissors beats Paper.";
+    }
+    else if (computerSelection === "Rock"){
+        playerWins++;
+        return "You Win! Paper beats Rock";
+    }
+    else
+        return "Draw! Go Again.";
+}
+
+function scissorsLogic(){
+    if (computerSelection === "Rock"){
+        computerWins++;
+        return "You Lose! Rock beats Scissors.";
+    }
+    else if (computerSelection === "Paper"){
+        playerWins++;
+        return "You Win! Scissors beats Paper";
+    }
+    else
+        return "Draw! Go Again.";
 }
 
 function checkIfWinner(){
     if (playerWins === 5 || computerWins === 5){
         victory.textContent = determineWinner();
         results.appendChild(victory);
+        btns.forEach(btn => btn.disabled = true);
+        createResetButton();
     }
 
 }
@@ -84,6 +94,24 @@ function determineWinner(){
         return "Congrats you won!"
     else
         return "Computer reigns supreme!"
+}
+
+function createResetButton(){
+    const resetButton = document.createElement('button');
+    resetButton.textContent = 'Go Again?';
+    resetButton.classList.add("reset");
+    results.appendChild(resetButton);
+    const reset = document.querySelector('.reset');
+    reset.addEventListener('click', () => resetGame());
+}
+
+function resetGame(){
+    playerWins = 0;
+    computerWins = 0;
+    btns.forEach(btn => btn.disabled = false);
+    results.replaceChildren();
+    roundResult.textContent = 'Make your choice';
+    results.appendChild(roundResult);
 }
 
 const btns = document.querySelectorAll('.choice');
